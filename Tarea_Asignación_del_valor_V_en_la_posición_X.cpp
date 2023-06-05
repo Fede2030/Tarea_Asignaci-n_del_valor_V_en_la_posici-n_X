32d48d2ad172db370a92e8e48487a0aa5942b99a
Fede2030 <133687412+Fede2030@users.noreply.github.com>
2023-06-05 10:14:28 -0600

    Programa Finalizado

--- Tarea_Asignación_del_valor_V_en_la_posición_X.cpp
+++ Tarea_Asignación_del_valor_V_en_la_posición_X.cpp
@@ -1,49 +1,91 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
 
-// Funcion para asignar un valor en la posicion X del arreglo
-void asignarValor(int* arreglo, int posicion, int valor)
+class RawArray {
+private:
+    int* arreglo;
+    int size;
+
+public:
+    RawArray(int in_Size);
+    void asignarValor(int posicion, int valor);
+    int accederElemento(int posicion) const;
+    void imprimirArreglo() const;
+    void inicializarArreglo();
+};
+
+RawArray::RawArray(int in_Size) : size(in_Size)
 {
-	arreglo[posicion] = valor;
+    arreglo = new int[size];
 }
 
-// Funcion para acceder al elemento en la posicion solicitada del arreglo
-int accederElemento(const int* arreglo, int posicion)
+void RawArray::asignarValor(int posicion, int valor)
 {
-	return arreglo[posicion];
+    arreglo[posicion] = valor;
 }
 
-int main()s
+int RawArray::accederElemento(int posicion) const
 {
-	// Definir el raw array inicial
-	int arreglo[100];
-	memset(arreglo, -1, sizeof(arreglo));
-	// Solicitar la posicion y el valor al usuario
-	int posicion, valor;
-	std::cout << "ingrese la posicion:";
-	std::cin >> posicion;
-	std::cout << "ingrese el valor:";
-	std::cin >> valor;
+    return arreglo[posicion];
+    //return * (arreglo + posicion); son equivalentes 
+}
 
-	//Asiganr el valor V en la posicion X 
-	asignarValor(arreglo, posicion, valor);
+void RawArray::imprimirArreglo() const
+{
+    std::cout << "Arreglo actualizado: ";
+    int* ptr = arreglo;
+    for (int i = 0; i < size; i++) {
+        std::cout << *(ptr + i) << " ";
+    }
+    std::cout << std::endl;
+}
 
-	//Acceder al elemento en la posicion solicitada
-	int elemento = accederElemento(arreglo, posicion);
+void RawArray::inicializarArreglo()
+{
+    int* ptr = arreglo;
+    for (int i = 0; i < size; i++) {
+        *(ptr + i) = i;
+    }
+}
 
-	// Imprimir el arreglo actualizado y el elemento asignado
-	std::cout << "\nArreglo actualizado:\n";
-	for (int i = 0; i < 100; i++)
-	{
-		if (arreglo[i] == -1)
-		{
-			std::cout << i << " ";
-		}
-		else {
-			std::cout << arreglo[i] << " ";
-		}
-	}
-	std::cout << std::endl;
-	std::cout << "Elemento en la posicion " << posicion << ": " << elemento << std::endl;
-	return 0;
+int main()
+{
+    int size;
+    std::cout << "Ingrese el tamaño del arreglo: ";
+    std::cin >> size;
+
+    RawArray rawArray(size);
+
+    // Inicializar el arreglo con valores iniciales
+    rawArray.inicializarArreglo();
+
+    int posicion, valor;
+    bool posicionValida = false;
+
+    do {
+        std::cout << "Ingrese la posicion (menor al tamaño del arreglo): ";
+        std::cin >> posicion;
+
+        if (posicion < size) {
+            posicionValida = true;
+        }
+        else {
+            std::cout << "Ingrese un número menor al tamaño del arreglo." << std::endl;
+        }
+    } while (!posicionValida);
+
+    std::cout << "Ingrese el valor: ";
+    std::cin >> valor;
+
+    // Asignar el valor en la posición solicitada
+    rawArray.asignarValor(posicion, valor);
+
+    // Acceder al elemento en la posición solicitada
+    int elemento = rawArray.accederElemento(posicion);
+
+    // Imprimir el arreglo actualizado
+    rawArray.imprimirArreglo();
+
+    std::cout << "Elemento en la posicion " << posicion << ": " << elemento << std::endl;
+
+    return 0;
 }
